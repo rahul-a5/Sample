c89e82a07ba9b83259a37885dc8fb62dba03d498
rahul-a5 <myrahulanand20@gmail.com>
2020-10-15 16:41:17 +0530

    Negative number root

--- sqrt.c
+++ sqrt.c
@@ -14,11 +14,10 @@ int main(int argc, char* argv[]) {
 
 	int input = atoi(argv[1]);
 	if(input>=0){
-	printf("Valid no\n");
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	}
 	else{
-	printf("Invalid no\n");
+	printf("Sqrt of %d is %fi\n",input,sqrt(-1*input));
 	}
 	printf("End of program. Exiting.");
 	return(0);
